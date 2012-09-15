da493525b648c3466cf95cd30e631f26f49b7ce8
Ionut Bizau <i@ibz.me>
2012-09-15 18:49:42 +0200

    Update last.fm fetch.

--- lastfm_fetch.py
+++ lastfm_fetch.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python
 
-__doc__ = """Usage: %s username""" % __file__
+__doc__ = """Usage: %s username password""" % __file__
 
+import cookielib
 import gzip
 import sys
 from StringIO import StringIO
 import time
+import urllib
 import urllib2
 
 from lxml.html import parse
 
+jar = cookielib.CookieJar()
+
 try:
     from urlgrabber.keepalive import HTTPHandler
-    opener = urllib2.build_opener(HTTPHandler())
+    opener = urllib2.build_opener(HTTPHandler(urllib2.HTTPCookieProcessor(jar)))
 except ImportError:
     sys.stderr.write("WARNING: urlgrabber not available. Will not use keepalive.\n")
-    opener = urllib2.build_opener()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
 
 def parse_xml(url):
     request = urllib2.Request(url, None, {"Accept-encoding": "gzip"})
     response = gzip.GzipFile(fileobj=StringIO(opener.open(request).read()))
-    return parse(response).getroot()
+    return parse(StringIO(response.read().decode('utf-8'))).getroot()
 
 def scrape_page(url):
     doc = parse_xml(url)
     for row in reversed(doc.cssselect("table.tracklist tr")):
         try:
             artist, title = [a.text.strip() for a in row.cssselect("td.subjectCell a")]
-            timestamp = row.cssselect("td.dateCell abbr")[0].attrib['title']
+            timestamp = row.cssselect("td.dateCell time")[0].attrib['datetime']
             yield artist, title, timestamp
         except Exception:
             pass
@@ -39,17 +43,24 @@ def scrape_user(url):
     except:
         page_count = 1
     for i in reversed(range(1, page_count + 1)):
+        sys.stderr.write("page %s/%s...\n" % (i, page_count))
+        time.sleep(0.5)
         for artist, title, timestamp in scrape_page("%s?page=%s" % (url, i)):
             yield artist, title, timestamp
 
-def main(username):
-    url = "http://last.fm/user/%s/tracks" % username
+def login(username, password):
+    opener.open("https://www.last.fm/login", urllib.urlencode([('username', username), ('password', password)]))
+
+def main(username, password):
+    login(username, password)
+
+    url = "http://www.last.fm/user/%s/tracks" % username
     for artist, title, timestamp in scrape_user(url):
         sys.stdout.write((u"%s\t%s\t%s\n" % (artist, title, timestamp)).encode("utf-8"))
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        username = sys.argv[1]
-        main(username)
+    if len(sys.argv) == 3:
+        username, password = sys.argv[1:]
+        main(username, password)
     else:
         print __doc__
